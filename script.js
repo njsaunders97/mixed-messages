183168b41b590dc3863ff94ffe44138d1f89182e
njsaunders97 <njsaunders97@gmail.com>
2024-06-19 15:14:07 +0200

    Creates data structures

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("test");
+const messageParts = {
+    part1: [
+        "You can do",
+        "You're capable of",
+        "Believe in",
+        "Never give up on",
+        "Keep focusing on",
+    ],
+    part2: [
+        "anything you put your mind to",
+        "all of the things you can imagine",
+        "the journey of your dreams",
+        "the path that lies in front of you"
+    ],
+    part3: [
+        "champ",
+        "friend",
+        "buddy, and you have a fat bunda",
+        "sport"
+    ]
+}
+
