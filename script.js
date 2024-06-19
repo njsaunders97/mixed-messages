07d4e301fd78d157bea2e675458e29089e175a41
njsaunders97 <njsaunders97@gmail.com>
2024-06-19 15:25:30 +0200

    Added mixed message functions

--- script.js
+++ script.js
@@ -7,16 +7,31 @@ const messageParts = {
         "Keep focusing on",
     ],
     part2: [
-        "anything you put your mind to",
-        "all of the things you can imagine",
-        "the journey of your dreams",
-        "the path that lies in front of you"
+        "anything you put your mind to,",
+        "all of the things you can imagine,",
+        "the journey of your dreams,",
+        "the path that lies in front of you,"
     ],
     part3: [
-        "champ",
-        "friend",
-        "buddy, and you have a fat bunda",
-        "sport"
+        "champ.",
+        "friend.",
+        "buddy. Remember you have a fat bunda!",
+        "sport."
     ]
 }
 
+
+const getRandomElement = array => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+const generateMessage = () => {
+    const part1 = getRandomElement(messageParts.part1);
+    const part2 = getRandomElement(messageParts.part2);
+    const part3 = getRandomElement(messageParts.part3);
+
+    return `${part1} ${part2} ${part3}`;
+}
+
+console.log(generateMessage());
